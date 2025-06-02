d86df6abb37e9e1d0cf1dd31c665fae36804ede6
Saurab Thapa <saurabthapa@karunya.edu.in>
2025-06-02 18:51:19 +0530

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,14 +1,19 @@
 import random
-guess=int(input("Guess a number between 1 and 10: "))
-chance=3
-orig_num=random.randint(1,10)
+guess=int(input("Guess a number between 1 and 100: "))
+chance=4
+orig_num=random.randint(1,100)
 while(chance>1):
-    chance-=1
     if(guess==orig_num):
         print("Bingo!\nYou guessed it right...")
-    else:
-        print("Oops! Might need to try again")
+        break
+    if(guess>orig_num):
+        print("Too high!\nTry Again")
         print("Chances remaining : ",chance)
         guess=int(input("Guess again:"))
+    if(guess<orig_num):
+        print("Too low!\nTry Again")
+        print("Chances remaining : ",chance)
+        guess=int(input("Guess again:"))
+    chance-=1
 
 print("The number was :  ",orig_num)
